3efe945fd16035f2b9c3f12a855ef8c7d037c058
Mario Alejandro Gonzalez Cardona <alejandrogonzalez@uadec.edu.mx>
2025-08-11 12:29:51 -0600

    eliminacion de ngrok

--- ngrok.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Agrega el authtoken de ngrok (solo es necesario la primera vez)
-ngrok config add-authtoken 30QorNduncj5jeESnfuWQxo9NSQ_mYeuve81PWBdzibgLg1A
-
-# Inicia el túnel a localhost:8080
-ngrok http http://localhost:8080
